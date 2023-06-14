a7d7888ef8a996f04dc6dc0309505d80e55b5e88
suganyasuku <61615582+suganyasuku@users.noreply.github.com>
2023-06-14 12:22:08 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -7,6 +7,7 @@ public class Sample {
 	public static void main(String[] arg) {
 		System.setProperty("webdriver.chrome.driver", "D:\\projects\\first project\\Chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.close();
 		
 	}
 
